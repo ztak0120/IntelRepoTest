58a6ccced6a2bf27d05fad97dddda65ada7cdef1
ztak0120 <86049467+ztak0120@users.noreply.github.com>
2022-12-01 16:28:22 +0530

    Update synch.sh
    update

--- synch.sh
+++ synch.sh
@@ -7,7 +7,6 @@ echo "send file"
 #GIT_SSH_COMMAND="ssh" git clone ssh://git-codecommit.us-east-2.amazonaws.com/v1/repos/MyDemoRepo my-demo-repo
 #DEST_REPO='https://ak0120+2-at-547321245783:516+zKNi6OIbOZLH07zrqy6XAj4PTFgnm2KZeqamL84=@git-codecommit.ap-south-1.amazonaws.com/v1/repos/zt-123_test'
 #SRC_REPO='https://ztak0120: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@github.com/ztak0120/123-REPO-TEST.git'
-
 rm -rf $CLONE_DIR
 mkdir $Intel_DIR
 #git clone --mirror "$SRC_REPO" $CLONE_DIR
