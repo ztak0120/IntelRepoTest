db13b3b159c36ae42027af23a202eedf3c69a385
ztak0120 <86049467+ztak0120@users.noreply.github.com>
2022-12-02 16:13:39 +0530

    SK

--- synch.sh
+++ synch.sh
@@ -8,7 +8,7 @@ pwd
 ls -la
 #GIT_SSH_COMMAND="ssh" git clone ssh://git-codecommit.us-east-2.amazonaws.com/v1/repos/MyDemoRepo my-demo-repo
 DEST_REPO='https://ak0120+2-at-547321245783:516+zKNi6OIbOZLH07zrqy6XAj4PTFgnm2KZeqamL84=@git-codecommit.ap-south-1.amazonaws.com/v1/repos/zt-123_test'
-SRC_REPO='https://ztak0120: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@github.com/ztak0120/123-REPO-TEST.git'
+SRC_REPO='https://ztak0120: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@github.com/ztak0120/IntelRepoTest.git'
 rm -rf $CLONE_DIR
 mkdir $CLONE_DIR
 git clone --mirror "$SRC_REPO" $CLONE_DIR
